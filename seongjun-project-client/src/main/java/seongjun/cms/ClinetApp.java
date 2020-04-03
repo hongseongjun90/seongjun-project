9e529873b41f529f4eb16bae66a4e7a85d6d0435
hongseongjun90 <hongseongjun90@gmail.com>
2020-04-03 16:40:11 +0900

    v24

---
+++ seongjun-project-client/src/main/java/seongjun/cms/ClinetApp.java
@@ -0,0 +1,7 @@
+package seongjun.cms;
+
+public class ClinetApp {
+  public static void main(String[] args) {
+    System.out.println("클라이언트 커뮤니티 관리 시스템입니다.");
+  }
+}
---
+++ seongjun-project-server/src/main/java/seongjun/cms/ServerApp.java
@@ -0,0 +1,7 @@
+package seongjun.cms;
+
+public class ServerApp {
+  public static void main(String[] args) {
+    System.out.println("서버 커뮤니티 관리 시스템입니다.");
+  }
+}
